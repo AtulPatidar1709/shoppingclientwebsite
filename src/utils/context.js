4f41b3722af7467f7b38fec73720edc7538f1836
Atul Patidar <48468580+AtulPatidar1709@users.noreply.github.com>
2023-02-13 12:47:05 +0530

    Donne changes in SRC

---
+++ src/utils/context.js
@@ -0,0 +1,9 @@
+import { createContext, useState } from "react";
+
+export const context = createContext();
+
+const AppContext = ({ children }) => {
+  return <context.Provider>{children}</context.Provider>;
+};
+
+export default AppContext;
